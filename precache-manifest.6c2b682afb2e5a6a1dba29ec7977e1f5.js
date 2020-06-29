320bb619769585635e3f3e45c577f7dcb6987cf6
irenekyc <gugu_irene@hotmail.com>
2020-06-29 13:13:02 +0200

    Updates

---
+++ precache-manifest.6c2b682afb2e5a6a1dba29ec7977e1f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3292e7025e26437d6ed912173487e600",
+    "url": "/simple-page-theme-switch/index.html"
+  },
+  {
+    "revision": "bd1489ec728f0464945e",
+    "url": "/simple-page-theme-switch/static/js/2.73208e02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-page-theme-switch/static/js/2.73208e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31cb7ada8bd942e4e0b",
+    "url": "/simple-page-theme-switch/static/js/main.9590cce2.chunk.js"
+  },
+  {
+    "revision": "46356cc8a113efbefdd9",
+    "url": "/simple-page-theme-switch/static/js/runtime-main.c3af0b92.js"
+  }
+]);
